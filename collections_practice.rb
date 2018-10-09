5cb3196589fc374d7ab5c1829c2f7120ad8aa251
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-10-09 03:07:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
 # your code goes here
+def begins_with_r(array)
+  array.sort do |a, b|
+    if a.start_with?("r") && b.start_with?("r")
+      return true 
+   else return false 
+    end 
+  end 
+end 
+
+def contain_a(array)
+  contains_a = []
+  array.each do |a|
+    if a.include?("a") 
+       contains_a << a
+    end
+  end 
+  contains_a
+end 
+
+def first_wa(array)
+  array.find do |a|
+    a.to_s.start_with?("wa")
+  end 
+end 
+
+def remove_non_strings(array)
+  array.delete_if do |a|
+    a != a.to_s 
+  end 
+end 
+
+def count_elements(array)
+
+ end
+  
+def merge_data(keys, values)
+ keys.each 
+
+ 
+  end 
+  
+def find_cool
+  
